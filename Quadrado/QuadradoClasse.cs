b757226de7cccdf069ccd5f5ad069ea4654d22e9
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2017-05-17 12:20:04 -0300

    Adicionando a classlib Quadrado

---
+++ Quadrado/QuadradoClasse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quadrado
+{
+    public static class Quadrado
+    {
+        public static double Area(double lado)
+        {
+            return lado * lado;
+        }
+    }
+}
