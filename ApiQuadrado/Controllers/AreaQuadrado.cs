d4247945431f3204b28aff0822eeefb46f02e676
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2017-05-17 12:21:44 -0300

    Adicionando a API ApiQuadrado

---
+++ ApiQuadrado/Controllers/AreaQuadrado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Quadrado;
+
+namespace ApiQuadrado.Controllers
+{
+    [Route("api/[controller]")]
+    public class AreaQuadradoExemplo : Controller
+    {
+        [HttpGet("Lado/{lado}")]
+        public object Get(double lado)
+        {
+            return new
+                   {
+                       Lado = lado,
+                       //Não sei porque precisou de mais um Quadrado :-(
+                       area = Quadrado.Quadrado.Area(lado)
+                   };
+        }
+    }
+}
